2709d4a67d38cf516b4acaaba6452446942c6525
micha <micha.reichmann@gmail.com>
2016-04-05 12:17:42 +0200

    adding Utils file for general function with MOST IMPORTANT function print banner and adding a name to the tree

--- HiggsProjekt.py
+++ HiggsProjekt.py
@@ -6,19 +6,26 @@
 from ROOT import TFile
 from glob import glob
 from copy import deepcopy
-
+from Utils import *
 
 
 class Analsis:
     def __init__(self):
 
         self.DataFolder = 'l3higgs189/'
+
+        # dictionary of the tree name and the tree
         self.trees = self.load_trees()
 
     def load_trees(self):
         files = [TFile(f) for f in glob('{dir}*.root'.format(dir=self.DataFolder))]
         trees = [f.Get('h20') for f in files]
-        return deepcopy(trees)
+        dic = {f.GetName().split('/')[-1].strip('.root'): t for f, t in zip(files, trees)}
+        return deepcopy(dic)
 
-if __name__ == "__main__":
+
+__author__ = 'micha'
+
+if __name__ == '__main__':
+    print_banner('STARTING HIGGS ANALYSIS')
     z = Analsis()
