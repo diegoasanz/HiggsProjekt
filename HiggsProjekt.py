eab22bb45c92db842aff59502b115f1255b7f8c5
Diego Alejandro Sanz Becerra <sandiego@phys.ethz.ch>
2016-04-05 11:32:06 +0200

    Second commit

--- HiggsProjekt.py
+++ HiggsProjekt.py
@@ -1,1 +1,17 @@
-from ROOT import TCanvas
+# ==============================================
+# IMPORTS
+# ==============================================
+from ROOT import TGraphErrors, TCanvas, TH2D, gStyle, TH1F, gROOT, TLegend, TCut, TGraph, TProfile2D, TH2F, TProfile, TCutG, kGreen, TF1, TPie
+from TelescopeAnalysis import Analysis
+from CurrentInfo import Currents
+from numpy import array
+from math import sqrt, ceil, log
+from argparse import ArgumentParser
+from Extrema import Extrema2D
+from ChannelCut import ChannelCut
+from time import time, sleep
+from collections import OrderedDict
+from sys import stdout
+from copy import deepcopy
+
+__author__ = 'DA'
