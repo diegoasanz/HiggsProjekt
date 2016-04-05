32b8251a076944826c0383b5a01d4c6aa8752a8f
micha <micha.reichmann@gmail.com>
2016-04-05 12:01:04 +0200

    adding an analyis class and a function to get all the trees

--- HiggsProjekt.py
+++ HiggsProjekt.py
@@ -1,1 +1,24 @@
-from ROOT import TCanvas
+# ---------------------------------------------------
+#           HIGGS PROJECT for SMATEP
+#   author: Michael Reichmann
+# ---------------------------------------------------
+
+from ROOT import TFile
+from glob import glob
+from copy import deepcopy
+
+
+
+class Analsis:
+    def __init__(self):
+
+        self.DataFolder = 'l3higgs189/'
+        self.trees = self.load_trees()
+
+    def load_trees(self):
+        files = [TFile(f) for f in glob('{dir}*.root'.format(dir=self.DataFolder))]
+        trees = [f.Get('h20') for f in files]
+        return deepcopy(trees)
+
+if __name__ == "__main__":
+    z = Analsis()
