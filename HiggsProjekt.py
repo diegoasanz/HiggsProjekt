04b4d90657224f3bea27c6821940fdb87237769e
Diego Alejandro Sanz Becerra <sandiego@phys.ethz.ch>
2016-04-05 18:01:16 +0200

    added comments to the class

--- HiggsProjekt.py
+++ HiggsProjekt.py
@@ -11,20 +11,40 @@ __author__ = 'Pin-Jung & Diego Alejandro'
 
 from Utils import *
 
-class Analsis:
+class Analysis:
+    # this method is the initialization of the Analysis class. This is the constructor of the Analysis class.
     def __init__(self):
+        '''
+        :param self: it menas that it takes itself as parameter to initialize
+        :return: this method does not return anything, it is just the constructor.
+        '''
 
+        # This is the folder name where the data is stored. Download it from the student forum page in moodle and save the data in the same path of this projekt
         self.DataFolder = 'l3higgs189/'
 
-        # dictionary of the tree name and the tree
+        # This calls the method 'load_trees' of the Analysis class, and the results are stored in the variable trees.
+        # trees variable will have a dictionary with the tree name and the tree
         self.trees = self.load_trees()
 
+    # This method loads the trees from the root file located at the DataFolder
     def load_trees(self):
+        # 'files' is a vector of TFile. Here 'glob' looks for any file that ends with .root and makes a list out of it.
+        # Each entry of the vector 'files', is a TFile that opens each of the root files inside the class' variable 'DataFoler'.
         files = [TFile(f) for f in glob('{dir}*.root'.format(dir=self.DataFolder))]
+        # 'trees' is a vector of TTree. Here for each element TFile in 'files', the tree 'h20' is extracted.
+        # each entry of 'trees' has the corresponding tree of the root file in 'files'
         trees = [f.Get('h20') for f in files]
+        # 'dic' is a Python dictionary that assigns each stem of the file name 'f' (from the vector 'files') with a
+        # tree 't' (from the vector 'trees')
         dic = {f.GetName().split('/')[-1].strip('.root'): t for f, t in zip(files, trees)}
+        # returns a complete copy of this dictionary. If it was not copied, as soon as the method exits, all the information would be lost. That
+        # is why, it needs to be 'deepcopied' so that this dictionary can be used outside this method.
         return deepcopy(dic)
 
+# This is the main that it is called if you start the python script
 if __name__ == '__main__':
+    # print_banner is located in Utils Class found in Utils.py. This Class is for usefull utilities.
+    # print_banner is a nice way to print the information in terminal
     print_banner('STARTING HIGGS ANALYSIS')
-    z = Analsis()
+    # this creates an instance (object) of the Analysis class that is defined above.
+    z = Analysis()
