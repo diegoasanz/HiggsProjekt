2f81155a1b5e0ffdb378664ae28e935ff4bd468b
micha <micha.reichmann@gmail.com>
2016-05-25 08:29:34 +0200

    adding method to make a TGraphErrors and adding some spaces

--- Utils.py
+++ Utils.py
@@ -1,16 +1,19 @@
 # utility functions
 
-from ROOT import gROOT, TCanvas
+from ROOT import gROOT, TCanvas, TGraphErrors
 import ROOT
 from datetime import datetime
 from termcolor import colored
+
 
 def log_warning(msg):
     t = datetime.now().strftime('%H:%M:%S')
     print '{head} {t} --> {msg}'.format(t=t, msg=msg, head=colored('WARNING:', 'red'))
 
+
 def print_banner(msg, symbol='='):
     print '\n{delim}\n{msg}\n{delim}\n'.format(delim=len(str(msg)) * symbol, msg=msg)
+
 
 def format_histo(histo, name='', title='', x_tit='', y_tit='', z_tit='', marker=20, color=1, markersize=1, x_off=1, y_off=1, z_off=1, lw=1, fill_color=0):
         h = histo
@@ -32,6 +35,7 @@ def format_histo(histo, name='', title='', x_tit='', y_tit='', z_tit='', marker=
         except AttributeError or ReferenceError:
             pass
 
+
 def save_plots(savename, save_dir, file_type=None,  sub_dir=None, canvas=None):
         save_dir = save_dir
         file_type = '.png' if file_type is None else '.{end}'.format(end=file_type)
@@ -50,6 +54,7 @@ def save_plots(savename, save_dir, file_type=None,  sub_dir=None, canvas=None):
         except Exception as inst:
             print_banner('ERROR in save plots! {inst}'.format(inst=inst))
 
+
 def save_histo(histo, save_name, show, save_dir, lm=.1, rm=0.1, draw_opt='', x=2000, y=2000, l=None):
         h = histo
         gROOT.SetBatch(1) if not show else do_nothing()
@@ -60,6 +65,18 @@ def save_histo(histo, save_name, show, save_dir, lm=.1, rm=0.1, draw_opt='', x=2
         save_plots(save_name, save_dir)
         gROOT.SetBatch(0)
         return [c, h, l] if l is not None else [c, h]
+
+
+def make_tgrapherrors(name, title, color=1, marker=20, marker_size=1, width=1):
+    gr = TGraphErrors()
+    gr.SetTitle(title)
+    gr.SetName(name)
+    gr.SetMarkerStyle(marker)
+    gr.SetMarkerColor(color)
+    gr.SetLineColor(color)
+    gr.SetMarkerSize(marker_size)
+    gr.SetLineWidth(width)
+    return gr
 
 
 def create_colorlist():
@@ -74,12 +91,14 @@ def create_colorlist():
 colorlist = create_colorlist()
 count = 0
 
+
 def get_color():
     global count
     count %= 20
     color = colorlist[count]
     count += 1
     return color
+
 
 def do_nothing():
     pass
